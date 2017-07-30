32d4dd21ac67c50c35af8ef908b85316ac9c19c2
Diogo Laginha <diogo.laginha.machado@gmail.com>
2017-07-30 15:55:34 +0100

    move store into a folder in the template

--- packages/react-scripts/template/src/routes/index.js
+++ packages/react-scripts/template/src/routes/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
 import { Switch, Route, Redirect } from 'react-router-dom';
-import history from '../history';
 import store from '../store';
+import history from '../store/history';
 // My components
 import App from '../containers/App';
 
---
+++ packages/react-scripts/template/src/store/README.md
@@ -0,0 +1,4 @@
+# Store
+
+Actions represent the facts about “what happened” and the reducers update the state according to those actions.
+The Store is the object that brings them together.
